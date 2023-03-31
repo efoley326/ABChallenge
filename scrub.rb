39471ca0264ee13a1cc0af66541393701a9bc466
Emily <efoley326@gmail.com>
2023-03-31 13:11:30 -0400

    program outline

--- scrub.rb
+++ scrub.rb
@@ -1,5 +1,15 @@
 require 'json'
 
-file = ARGF.read
-data_file = JSON.parse(file)
-puts data_file
+# return txt file as an array
+
+
+# sort filenames to return only json files
+
+
+# parse json
+
+
+# match json keys to sensitive fields && scrub fields' values
+
+
+# write scrubbed data to new json file
