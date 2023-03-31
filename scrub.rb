0d01e9d9a3de5be24c4dd073053efbdbe8efc42e
Emily <efoley326@gmail.com>
2023-03-31 13:17:53 -0400

    fields file into array

--- scrub.rb
+++ scrub.rb
@@ -1,8 +1,10 @@
 require 'json'
 
 # return txt file as an array
-
-
+sensitive_fields = File.open('sensitive_fields.txt', 'r').flat_map {
+  |field| field.split(' ') 
+}
+puts sensitive_fields
 # sort filenames to return only json files
 
 
